c56298e5b52a8e53c3031598c5889488368cfecb
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2015-05-17 01:09:27 -0300

    Transformando o IP em variável.

--- Connect.ps1
+++ Connect.ps1
@@ -1,5 +1,5 @@
-﻿#Set-Item WSMan:\localhost\Client\TrustedHosts '192.168.56.101'
+﻿$ip = '192.168.56.101'
+Set-Item WSMan:\localhost\Client\TrustedHosts $ip
 $password = ConvertTo-SecureString "administrator" -AsPlainText -Force
 $cred= New-Object System.Management.Automation.PSCredential ("administrator", $password )
-
-Enter-PSSession -ComputerName '192.168.56.101' -Credential $cred
+Enter-PSSession -ComputerName $ip -Credential $cred
